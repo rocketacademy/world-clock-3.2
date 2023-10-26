0eea14afd1ba5d915c6830a6a56ebd5efc4766b5
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-26 16:01:54 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from "/logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo" alt="Rocket logo" />
+      </div>
+      <h1>World Clock</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
