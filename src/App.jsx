cf5849119d432f02cc937d2a7f4c185214073bf9
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-27 11:08:12 +0800

    exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import logo from "/logo.png";
 import "./App.css";
-import Clock from "./Clock.jsx";
+// import Clock from "./Clock.jsx";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <img src={logo} className="logo" alt="Rocket logo" />
+//       </div>
+//       <h1>World Clock</h1>
+//       <Clock timeZone="America/Los_Angeles" />
+//       <Clock timeZone="Europe/London" />
+//       <Clock timeZone="Asia/Singapore" />
+//     </>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
   return (
-    <>
-      <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
-      </div>
-      <h1>World Clock</h1>
-      <Clock timeZone="America/Los_Angeles" />
-      <Clock timeZone="Europe/London" />
-      <Clock timeZone="Asia/Singapore" />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* Render date value that is stored in state */}
+        <p>{date.toString()}</p>
+      </header>
+    </div>
   );
 }
-
-export default App;
