676a45ad4d4791a62c9db9777389333f2fbe34d6
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-27 12:06:11 +0800

    aligning

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
 import logo from "/logo.png";
 import "./App.css";
-// import Clock from "./Clock.jsx";
+import Clock from "./Clock";
 
-// function App() {
-//   return (
-//     <>
-//       <div>
-//         <img src={logo} className="logo" alt="Rocket logo" />
-//       </div>
-//       <h1>World Clock</h1>
-//       <Clock timeZone="America/Los_Angeles" />
-//       <Clock timeZone="Europe/London" />
-//       <Clock timeZone="Asia/Singapore" />
-//     </>
-//   );
-// }
-
-// export default App;
-
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* Render date value that is stored in state */}
-        <p>{date.toString()}</p>
-      </header>
-    </div>
+    <>
+      <div>
+        <img src={logo} className="logo" alt="Rocket logo" />
+      </div>
+      <h1>World Clock</h1>
+      <div className="card">
+        <Clock timeZone="Asia/Singapore" />
+        <Clock timeZone="Europe/London" />
+        <Clock timeZone="Asia/Tokyo" />
+      </div>
+    </>
   );
 }
+
+export default App;
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,28 +1,32 @@
+// import required React hooks for this component, useState will implement state, where as useEffect facilitates implementation of LifeCycle methods
 import { useState, useEffect } from "react";
 
 export default function Clock(props) {
+  // Initialise Component state to contain a "date" attribute with current date using the useState method.
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
+    // useEffect implementation to execute code when the component loads
     // Set date value in state every second to current date and time
-    // Save setInterval timer ID in class variable for teardown in another class method
-
+    // Save setInterval timerId in a variable for teardown later in the useEffect method
     const timerId = setInterval(() => {
       setDate(new Date());
     }, 1000);
 
-    // Teardown setInterval timer with timer ID saved as class variable
+    // useEffect implemenation to tear down the interval saved in timerId
     return () => {
+      // Teardown setInterval timer saved within the timerId variable
       clearInterval(timerId);
     };
   });
 
   return (
     // Render date value that is stored in state
-    <p>
-      {`${props.timeZone}: ${date.toLocaleString("en-GB", {
-        timeZone: props.timeZone,
-      })}`}
-    </p>
+    <>
+      <p>
+        {props.timeZone} :{" "}
+        {date.toLocaleString("en-GB", { timeZone: `${props.timeZone}` })}
+      </p>
+    </>
   );
 }
