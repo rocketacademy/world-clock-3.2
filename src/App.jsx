7e701e2ca28cfe066ab7be538df51da78781c072
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-27 11:20:22 +0800

    adding example repo where interval is in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import logo from "/logo.png";
 import "./App.css";
 
+// import required React hooks for this component, useState will implement state, where as useEffect facilitates implementation of LifeCycle methods
+import { useState, useEffect } from "react";
+
 function App() {
+  // Initialise Component state to contain a "date" attribute with current date using the useState method.
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    // useEffect implementation to execute code when the component loads
+    // Set date value in state every second to current date and time
+    // Save setInterval timerId in a variable for teardown later in the useEffect method
+    const timerId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    // useEffect implemenation to tear down the interval saved in timerId
+    return () => {
+      // Teardown setInterval timer saved within the timerId variable
+      clearInterval(timerId);
+    };
+  });
+
   return (
     <>
       <div>
@@ -9,9 +30,8 @@ function App() {
       </div>
       <h1>World Clock</h1>
       <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {/* Render date value that is stored in state */}
+        <p>{date.toString()}</p>
       </div>
     </>
   );
