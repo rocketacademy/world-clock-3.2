12ae3143d7900ee7cd8735863978acb23f494fc1
karenhnlai <karenhnlai@gmail.com>
2023-12-14 04:09:55 +0800

    World Clock Submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import logo from "/logo.png";
 import "./App.css";
+// import Clock component
+import Clock from "./Clock.jsx";
 
 function App() {
   return (
     <>
       <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
+        <img src={logo} className="logo" alt="Clock logo" />
       </div>
-      <h1>World Clock</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="App">
+        <header className="App-header">
+          <Clock timeZone="Asia/Hong_Kong" />
+          <Clock timeZone="Europe/Oslo" />
+          <Clock timeZone="America/Anchorage" />
+          <Clock timeZone="Pacific/Kiritimati" />
+        </header>
       </div>
     </>
   );
